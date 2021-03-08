18edd03901a9b4adada09173998ecea962a3d3cb
Kristian Hebert <khh@fiftytwo.com>
2021-03-08 11:41:35 +0100

    Added ContactPoint model
    #307

---
+++ Models/ContactPoint/ContactInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace OneTooX.DigitalPost.Model.ContactPoint
+{
+    public class ContactInfo
+    {
+        public string Label { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Models/ContactPoint/ContactPoint.cs
@@ -0,0 +1,11 @@
+﻿namespace OneTooX.DigitalPost.Model.ContactPoint
+{
+    public class ContactPoint
+    {
+        public string ContactPointId { get; set; }
+        public string ContactPointGroup { get; set; }
+        public string Label { get; set; }
+        public ContactInfo ContactInfo1 { get; set; }
+        public ContactInfo ContactInfo2 { get; set; }
+    }
+}
