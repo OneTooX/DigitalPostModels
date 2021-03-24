10eac4ba850351db8602d5c01b70fffc15684005
Kristian Hebert <khh@fiftytwo.com>
2021-03-24 15:05:14 +0100

    Support for 0-10 AdditionalContentData
    #295

--- Models/ContentData/ContentData.cs
+++ Models/ContentData/ContentData.cs
@@ -9,7 +9,16 @@
         public MotorVehicle MotorVehicle { get; set; }
         public string PropertyNumber { get; set; }
         public CaseId CaseId { get; set; }
-        public AdditionalContentData AdditionalContentData { get; set; }
+        public AdditionalContentData AdditionalContentData1 { get; set; }
+        public AdditionalContentData AdditionalContentData2 { get; set; }
+        public AdditionalContentData AdditionalContentData3 { get; set; }
+        public AdditionalContentData AdditionalContentData4 { get; set; }
+        public AdditionalContentData AdditionalContentData5 { get; set; }
+        public AdditionalContentData AdditionalContentData6 { get; set; }
+        public AdditionalContentData AdditionalContentData7 { get; set; }
+        public AdditionalContentData AdditionalContentData8 { get; set; }
+        public AdditionalContentData AdditionalContentData9 { get; set; }
+        public AdditionalContentData AdditionalContentData10 { get; set; }
         public KleData KleData { get; set; }
         public FormData FormData { get; set; }
         public ProductionUnit ProductionUnit { get; set; }
