ebac6741c9006de030676e578a4931e0ecabe937
Kristian Hebert <khh@fiftytwo.com>
2021-10-11 14:28:22 +0200

    Added BusinessReceipt model
    #500

---
+++ Models/Receipt/BusinessReceipt.cs
@@ -0,0 +1,16 @@
+﻿namespace OneTooX.DigitalPost.Model.Receipt
+{
+    using System;
+
+    public class BusinessReceipt
+    {
+        public Guid TransmissionId { get; set; }
+        public Guid MessageUuid { get; set; }
+        public string ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string ReceiptStatus { get; set; }
+        public override string ToString() =>
+            $"{nameof(BusinessReceipt)}{{{nameof(TransmissionId)}:{TransmissionId}, {nameof(MessageUuid)}:{MessageUuid}, {nameof(ErrorCode)}:{ErrorCode}, {nameof(ErrorMessage)}:{ErrorMessage}, {nameof(Timestamp)}:{Timestamp}, {nameof(ReceiptStatus)}:{ReceiptStatus}}}";
+    }
+}
