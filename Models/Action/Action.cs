5e46d2fc0d9942270b599a754d6995ecc2552579
Kristian Hebert <khh@fiftytwo.com>
2022-01-19 16:33:58 +0100

    Action times are now nullable
    #588

--- Models/Action/Action.cs
+++ Models/Action/Action.cs
@@ -6,8 +6,8 @@
     {
         public string Label { get; set; }
         public string ActionCode { get; set; }
-        public DateTimeOffset StartDateTime { get; set; }
-        public DateTimeOffset EndDateTime { get; set; }
+        public DateTimeOffset? StartDateTime { get; set; }
+        public DateTimeOffset? EndDateTime { get; set; }
         public EntryPoint EntryPoint { get; set; }
         public Reservation Reservation { get; set; }
     }
