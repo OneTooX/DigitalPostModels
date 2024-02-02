1c21962cd387376154cae94a14f63f120cb41890
Henning Gammelgaard Jensen <hgj@fiftytwo.com>
2024-02-02 12:23:56 +0100

    Added ReplyData and AdditionalReplyData.

---
+++ Models/ReplyData/AdditionalReplyData.cs
@@ -0,0 +1,8 @@
+﻿namespace OneTooX.DigitalPost.Model.ReplyData
+{
+    public class AdditionalReplyData
+    {
+        public string Label { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Models/ReplyData/ReplyData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OneTooX.DigitalPost.Model.ReplyData
+{
+    public class ReplyData
+    {
+        public string MessageId { get; set; }
+        public Guid MessageUuid { get; set; }
+        public Guid ReplyUuid { get; set; }
+        public string SenderId { get; set; }
+        public string RecipientId { get; set; }
+        public string CaseId { get; set; }
+        public string ContactPointId { get; set; }
+        public string GeneratingSystem { get; set; }
+        public string Comment { get; set; }
+        public AdditionalReplyData AdditionalReplyData1 { get; set; }
+        public AdditionalReplyData AdditionalReplyData2 { get; set; }
+        public AdditionalReplyData AdditionalReplyData3 { get; set; }
+        public AdditionalReplyData AdditionalReplyData4 { get; set; }
+    }
+}
